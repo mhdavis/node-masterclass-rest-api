f44f71b88aa5c1be56976bf7789d6525c01617d0
Michael Davis <mhdavis1993@gmail.com>
2018-08-02 10:39:22 -0400

    Added server callback to parse incoming API request url paths

--- index.js
+++ index.js
@@ -5,10 +5,22 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+    
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+s/g, ''); // removes extrenous slashes
+
+    // Send the response
     res.end('hello world\n');
+
+    // Log the request path
+    console.log(`Request received on path: ${trimmedPath}`)
 });
 
 // Star the server, and have it listen on port 3000
