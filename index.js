471f83d179ca2e8bf6db6037a00f9dbb9aa5cab3
Michael Davis <mhdavis1993@gmail.com>
2018-08-02 11:49:45 -0400

    Streaming in a payload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
@@ -24,17 +25,29 @@ var server = http.createServer(function(req, res) {
 
 
     // Get the Headers as an object:
-    var headers = req.headers
+    var headers = req.headers;
 
-    // Send the response
-    res.end('Response Sent!\n');
+    // Get Payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function (data) {
+        buffer += decoder.write(data); // as data streams in, adds decoded data to buffer string
+    });
 
-    // Log the request path
-    console.log('=====================================================')
-    console.log(`Request received on path: ${trimmedPath}`)
-    console.log(`Method: ${method}`)
-    console.log('Query String Parameters: ', JSON.stringify(queryStringObject))
-    console.log('Headers: ', JSON.stringify(headers))
+    req.on('end', function () {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('Response Sent!\n');
+
+        // Log the request path
+        console.log('=====================================================');
+        console.log(`Request received on path: ${trimmedPath}`);
+        console.log(`Method: ${method}`);
+        console.log('Query String Parameters: ', JSON.stringify(queryStringObject));
+        console.log('Headers: ', JSON.stringify(headers));
+        console.log(`Payload: ${buffer}`);
+    })
 });
 
 // Star the server, and have it listen on port 3000
