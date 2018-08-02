aa427e0ba143adeca8d2aafd2462e57d56bcdfb2
Michael Davis <mhdavis1993@gmail.com>
2018-08-02 15:21:11 -0400

    Added https support

--- index.js
+++ index.js
@@ -5,15 +5,42 @@
 
 // Dependencies
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder
 var config = require('./config');
+var fs = require('fs');
 
-// The server should respond to all requests with a string
-var server = http.createServer(function(req, res) {
+// Instantiating the HTTP server
+var httpServer = http.createServer(function(req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the HTTP server, and have it listen on environment port
+httpServer.listen(config.httpPort, function () {
+    console.log(`The server is listening on port ${config.httpPort} now in ${config.envName} mode`)
+})
+
+// Instantiate the HTTPS server
+var httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem')
+};
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, function () {
+    console.log(`The server is listening on port ${config.httpsPort} now in ${config.envName} mode`)
+})
+
+// All the server logic for both http and https server
+var unifiedServer = function(req, res) {
+    
     // Get the URL and parse it
     var parsedUrl = url.parse(req.url, true);
-    
+
     // Get the path
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, ''); // removes extrenous slashes
@@ -39,7 +66,7 @@ var server = http.createServer(function(req, res) {
         buffer += decoder.end();
 
         // Choose the handler this request should go to. If one is not found, use the notFound handler
-        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+        var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
         // Construct data object to send to the handler
         var data = {
@@ -51,12 +78,12 @@ var server = http.createServer(function(req, res) {
         }
 
         // Route the request specfied in the router
-        chosenHandler(data, function(statusCode, payload) {
+        chosenHandler(data, function (statusCode, payload) {
             // Use the status code called back by the handler, or default to 200
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
 
             // Use the payload called back by the handler, or default to an empty object
-            payload = typeof(payload) == 'object' ? payload : {};
+            payload = typeof (payload) == 'object' ? payload : {};
 
             // Convert payload to a string
             var payloadString = JSON.stringify(payload);
@@ -72,12 +99,7 @@ var server = http.createServer(function(req, res) {
             console.log(`Payload String: ${payloadString}`);
         });
     });
-});
-
-// Star the server, and have it listen on environment port
-server.listen(config.port, function () {
-    console.log(`The server is listening on port ${config.port} now in ${config.envName} mode`)
-})
+}
 
 // Define our handlers
 var handlers = {}
