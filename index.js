c6e790867a47e673d6413c4587f03d131af137bf
Michael Davis <mhdavis1993@gmail.com>
2018-09-18 15:23:03 -0400

    checks CRUD methods added to handlers

--- index.js
+++ index.js
@@ -108,5 +108,6 @@ var router = {
     'ping': handlers.ping,
     'users': handlers.users,
     'notFound': handlers.notFound,
-    'tokens': handlers.tokens
+    'tokens': handlers.tokens,
+    'checks': handlers.checks
 };
